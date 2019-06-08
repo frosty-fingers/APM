c2bb2e4bbcb2864ee9b8c57d57052ef1495b1917
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-09 00:51:37 +1000

    compiles

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Product } from '../product';
 
@@ -11,7 +11,8 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product:  Product;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id')
@@ -26,5 +27,9 @@ export class ProductDetailComponent implements OnInit {
       'starRating': 3.2,
       'imageUrl': 'assets/images/rake.png'
     }
+  }
+
+  onBack(): void {
+    this.router.navigate(['./products']);
   }
 }
