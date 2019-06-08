2ecfe290c2facec2fa2a73f4947754c490ab4b01
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-08 17:39:42 +1000

    compiles

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,6 +11,7 @@ export class ProductListComponent {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
           "productId": 1,
@@ -62,5 +63,9 @@ export class ProductListComponent {
           "starRating": 4.6,
           "imageUrl": asset('controller')
         }
-      ];
+    ];
+    
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
