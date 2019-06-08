4195d911eb9075125b788166cba7ee0235bed2b1
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-08 17:34:30 +1000

    compiles

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,6 +9,8 @@ let asset: (x: string) => string =
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
           "productId": 1,
