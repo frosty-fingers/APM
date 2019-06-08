3153411279ec22ca407c661fc339f201a41da30e
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-09 00:41:53 +1000

    compiles

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Product } from '../product';
 
 @Component({
@@ -9,9 +11,20 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product:  Product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id')
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'I have a leaf rake',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/rake.png'
+    }
   }
-
 }
