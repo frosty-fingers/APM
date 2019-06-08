1cabb2a605b226672140eda36158ff4d26cc0e87
frosty-fingers <48306144+frosty-fingers@users.noreply.github.com>
2019-06-08 23:43:48 +1000

    compiles

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,13 +9,12 @@ import { ProductService } from './product.service';
 })
 export class ProductListComponent implements OnInit{
 
-    asset: (x: string) => string =
-    function(x:string): string { return 'assets/images/' + x + '.png'; };
-
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
+
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
@@ -26,62 +25,10 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: Product[];
-    products: Product[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2016",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": this.asset('rake')
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": this.asset('wheelbarrow')
-        },
-        {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2016",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": this.asset('hammer')
-        },
-        {
-          "productId": 8,
-          "productName": "Saw",
-          "productCode": "TBX-0022",
-          "releaseDate": "May 15, 2016",
-          "description": "15-inch steel blade hand saw",
-          "price": 11.55,
-          "starRating": 3.7,
-          "imageUrl": this.asset('saw')
-        },
-        {
-          "productId": 10,
-          "productName": "Video Game Controller",
-          "productCode": "GMG-0042",
-          "releaseDate": "October 15, 2015",
-          "description": "Standard two-button video game controller",
-          "price": 35.95,
-          "starRating": 4.6,
-          "imageUrl": this.asset('controller')
-        }
-    ];
+    products: Product[];
 
     constructor(private productService: ProductService) {
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+
     };
 
     performFilter(filterBy: string): Product[] {
@@ -99,6 +46,12 @@ export class ProductListComponent implements OnInit{
     };
 
     ngOnInit(): void {
-        console.log('In OnInit')
-    };
+      this.productService.getProducts().subscribe(
+        products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error => this.errorMessage = <any>error
+      );
+    }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,65 +1,33 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
 import { Product } from './product'
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
-    asset: (x: string) => string =
-    function(x:string): string { return 'assets/images/' + x + '.png'; };
+    private productUrl = 'api/products/products.json';
 
-    getProducts(): Product[] {
-        return [
-            {
-              "productId": 1,
-              "productName": "Leaf Rake",
-              "productCode": "GDN-0011",
-              "releaseDate": "March 19, 2016",
-              "description": "Leaf rake with 48-inch wooden handle.",
-              "price": 19.95,
-              "starRating": 3.2,
-              "imageUrl": this.asset('rake')
-            },
-            {
-              "productId": 2,
-              "productName": "Garden Cart",
-              "productCode": "GDN-0023",
-              "releaseDate": "March 18, 2016",
-              "description": "15 gallon capacity rolling garden cart",
-              "price": 32.99,
-              "starRating": 4.2,
-              "imageUrl": this.asset('wheelbarrow')
-            },
-            {
-              "productId": 5,
-              "productName": "Hammer",
-              "productCode": "TBX-0048",
-              "releaseDate": "May 21, 2016",
-              "description": "Curved claw steel hammer",
-              "price": 8.9,
-              "starRating": 4.8,
-              "imageUrl": this.asset('hammer')
-            },
-            {
-              "productId": 8,
-              "productName": "Saw",
-              "productCode": "TBX-0022",
-              "releaseDate": "May 15, 2016",
-              "description": "15-inch steel blade hand saw",
-              "price": 11.55,
-              "starRating": 3.7,
-              "imageUrl": this.asset('saw')
-            },
-            {
-              "productId": 10,
-              "productName": "Video Game Controller",
-              "productCode": "GMG-0042",
-              "releaseDate": "October 15, 2015",
-              "description": "Standard two-button video game controller",
-              "price": 35.95,
-              "starRating": 4.6,
-              "imageUrl": this.asset('controller')
-            }
-        ];
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<Product[]>{
+        return this.http.get<Product[]>(this.productUrl).pipe(
+            tap(data => console.log('All: ' + JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occured: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code ${err.status}, error message is: ${err.message}`
+        }
+        console.log(errorMessage);
+        return throwError(errorMessage);
     }
 }
